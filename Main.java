46869a5627d73acad947c18c68964ccfeb6f3837
akrzysik <95127620+akrzysik@users.noreply.github.com>
2022-01-31 17:55:54 +0000

    oznacz zrobione

--- Main.java
+++ Main.java
@@ -21,11 +21,13 @@ public class Main {
 }
 
 /*    1 Rozbuduj przykładowy system o następujące elementy:
-        1.1 Dodawanie książek.
+        + 1.1 Dodawanie książek.
         1.2 Usuwanie czytelników.
-        1.3 Usuwanie książek.
-        1.4 Zwrot książki.
-        1.5 Wyświetlanie informacji o koncie czytelnika (o wypożyczonych przez niego książkach).
+        - analogiczne do usuwania książek, więc bez sensu robić
+        + 1.3 Usuwanie książek.
+        + 1.4 Zwrot książki.
+        + 1.5 Wyświetlanie informacji o koncie czytelnika (o wypożyczonych przez niego książkach).
         1.6 Wyświetlanie informacji o książce (komu wypożyczono poszczególne egzemplarze).
+        - analogiczne do inf. o koncie czytelnika, więc też bez sensu robić
     2 Dodaj mechanizm serializacji, tak aby możliwe było odtworzenie stanu systemu po jego ponownym uruchomieniu.
 */
