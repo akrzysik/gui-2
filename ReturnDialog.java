8df25ab241451dbe040208c3eac2aab8a27c7620
akrzysik <95127620+akrzysik@users.noreply.github.com>
2022-01-31 16:43:47 +0000

    zwroc ksiazke

--- ReturnDialog.java
+++ ReturnDialog.java
@@ -27,48 +27,29 @@ class ReturnDialog extends JDialog {
 
         add(Box.createRigidArea(new Dimension(0, 6)));
 
-        JLabel fname = new JLabel("Czytelnicy:");
+        JLabel fname = new JLabel("Wypożyczenia:");
         fname.setFont(new Font("Serif", Font.BOLD, 12));
         fname.setAlignmentX(0.5f);
         add(fname);
 
         add(Box.createRigidArea(new Dimension(0, 6)));
         
-        final JComboBox readersComboBox = new JComboBox(bib.getCzytelnicy().toArray());
-        readersComboBox.setSelectedIndex(-1);
-        readersComboBox.setPreferredSize(new Dimension(450, 22));
-        readersComboBox.setMaximumSize(new Dimension(450, 22));
-        add(readersComboBox);
+        final JComboBox selectComboBox = new JComboBox(bib.getWypozyczenia().toArray());
+        selectComboBox.setSelectedIndex(-1);
+        selectComboBox.setPreferredSize(new Dimension(450, 22));
+        selectComboBox.setMaximumSize(new Dimension(450, 22));
+        add(selectComboBox);
         
         add(Box.createRigidArea(new Dimension(0, 6)));
 
-        JButton showButton = new JButton("Pokaż wypożyczenia");
-        showButton.setAlignmentX(0.5f);
-        add(showButton);
-
-        JLabel lname = new JLabel("Ksiazki:");
-        lname.setFont(new Font("Serif", Font.BOLD, 12));
-        lname.setAlignmentX(0.5f);
-        add(lname);
-        
-        add(Box.createRigidArea(new Dimension(0, 6)));
-        
-        final JComboBox booksComboBox = new JComboBox(bib.getKsiazki().toArray());
-        booksComboBox.setSelectedIndex(-1);
-        booksComboBox.setPreferredSize(new Dimension(450, 22));
-        booksComboBox.setMaximumSize(new Dimension(450, 22));
-        //add(booksComboBox);
-
-        //add(Box.createRigidArea(new Dimension(0, 10)));
 
         JButton returnButton = new JButton("Zwróć");
         
         // Akcja podpieta pod przycisk "Show"
-        showButton.addActionListener(new ActionListener() {
+      returnButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent event) {
-              add(booksComboBox);
-
-            	
+            	bib.usunWypozyczenie(bib.getWypozyczenia().get(selectComboBox.getSelectedIndex()));
+                dispose();
             }
         });
 
